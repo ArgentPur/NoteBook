12794e58b1ecc82946f2108a1127463778604a59
Zachary Lynn <zalynn25@hotmail.com>
2020-11-09 11:24:53 -0500

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,15 @@ const router = require("express").Router();
 
 //Notes Route
 router.get("/notes", (req, res) => {
-    // here you will render the notes HTML page
+    // Renders the notes HTML page
     res.sendFile(path.join(__dirname, "../public/notes.html"));
-})
+});
 
 
 //All other Routes
 router.get("*", (req, res) => {
-    // this is the default route where you will render index.html
-}) 
+    // Default route to render index.html
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+}); 
 
 module.exports = router; 
